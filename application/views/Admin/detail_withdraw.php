496186e8119a6e09818a1309ce1c86f41ed835e4
Iwayanyogakrisna <oxygenjuno@gmail.com>
2021-09-12 19:12:48 +0800

    done

--- application/views/Admin/detail_withdraw.php
+++ application/views/Admin/detail_withdraw.php
@@ -41,12 +41,23 @@
                                     <b>Auto Save Property</b> <a id="auto_amount" class="float-right"></a>
                                 </li>
                                 <li class="list-group-item">
-                                    <b>USDT Wallet</b><button class="btn btn-info" onclick="myFunction()">Copy</button>
-                                    <input disabled class="form-control" id="usdt_wallet" value="">
+                                    <b>Status</b> <a id="status" class="float-right"></a>
+                                </li>
+                                <li class="list-group-item">
+                                    <b>USDT Wallet</b> <input disabled class="col-4 form-control" id="usdt_wallet" value=""> <button class="btn btn-info" onclick="myFunction()"><i class="fa fa-copy" aria-hidden="true"></i></button>
+                                    <!-- <div class="col-4">
+                                        <button class="btn btn-info" onclick="myFunction()"><i class="fa fa-copy" aria-hidden="true"></i></button>
+                                    </div>
+
+                                    </span>
+                                    <div class="col-8">
+                                        <input disabled class="form-control" id="usdt_wallet" value="">
+                                    </div> -->
+
                                 </li>
                             </ul>
-                            <button class="btn btn-warning btn-block"><b>Update Status to Finish</b></button>
-                            <button class="btn btn-danger btn-block"><b>Reject</b></button>
+                            <button onclick="update_status(2)" class="btn btn-warning btn-block"><b>Update Status to Finish</b></button>
+                            <button onclick="update_status(3)" class="btn btn-danger btn-block"><b>Reject</b></button>
 
                         </div>
                         <!-- /.card-body -->
@@ -54,7 +65,7 @@
 
                 </div>
                 <!-- /.col -->
-              
+
                 <!-- /.col -->
             </div>
             <!-- /.row -->
@@ -68,7 +79,37 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- <script src="<?php echo base_url() ?>assets/build/js/customer/Totalbonus.js"></script> -->
+<script>
+    function update_status(status) {
+        var id_order = document.getElementById('id_order').innerHTML;
+        var base_url = document.getElementById('base_url').innerHTML;
+        var vstatus = status;
+        $.ajax({
+            url: base_url + "api/update_status_withdraw",
+            type: "post",
+            data: {
+                'id': id_order,
+                'status': vstatus
+            },
+            success: function(result) {
+                $('.loader').attr('hidden', true);
+                //console.log('data : ' + result);
+                var d = JSON.parse(result);
+                show_message('success', d.response.message['english'], '');
+                location.reload();
 
+            },
+            error: function(result, ajaxOptions, thrownError) {
+                $('.loader').attr('hidden', true);
+                // console.log('data : '+xhr.responseText);
+                show_message('error', 'Oops! sepertinya ada kesalahan', 'kesalahan tidak diketahui');
+                var string = JSON.stringify(result.responseText);
+                var msg = JSON.parse(result.responseText);
+                show_message('error', 'Oops! sepertinya ada kesalahan', msg.response.message['english']);
+            }
+        });
+    }
+</script>
 <script>
     var id_order = document.getElementById('id_order').innerHTML;
     var base_url = document.getElementById('base_url').innerHTML;
@@ -87,6 +128,13 @@
             document.getElementById('withdraw_amount').innerHTML = d.data.transfer['withdraw_amount'];
             document.getElementById('name').innerHTML = d.data.user['name'];
             document.getElementById('auto_amount').innerHTML = d.data.transfer['auto_amount'];
+            if (d.data.withdraw['status'] == 1) {
+                document.getElementById('status').innerHTML = 'PENDING';
+            } else if (d.data.withdraw['status'] == 2) {
+                document.getElementById('status').innerHTML = 'ACCEPTED';
+            } else {
+                document.getElementById('status').innerHTML = 'REJECT';
+            }
             document.getElementById('usdt_wallet').value = d.data.user['usdt_wallet'];
         },
         error: function(result, ajaxOptions, thrownError) {
