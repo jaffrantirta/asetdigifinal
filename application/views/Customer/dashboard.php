802adc5f7f29dd1a55662cfe7551ea7e5bb85318
krisnaxyoga <yogakris.yk@gmail.com>
2021-09-15 22:32:40 -0700

    perbaiki layout dashboard

--- application/views/Customer/dashboard.php
+++ application/views/Customer/dashboard.php
@@ -239,13 +239,22 @@
       </div><!-- /.container-fluid -->
       <div class="container">
         <div class="row">
-          <div class="col-6">
-            <img width="200px" class="img-fluid" src="https://lintasdaerah.com/wp-content/uploads/2018/09/3-poin-utama-crypto-mata-uang-komoditas-token.jpg" alt="Los Angeles">
-          </div>
-          <div class="col-6">
-            <img width="200px" class="img-fluid" src="https://lintasdaerah.com/wp-content/uploads/2018/09/3-poin-utama-crypto-mata-uang-komoditas-token.jpg" alt="Los Angeles">
+          <div class="col-12">
+            <img width="919" height="100" class="rounded img-fluid" src="https://lintasdaerah.com/wp-content/uploads/2018/09/3-poin-utama-crypto-mata-uang-komoditas-token.jpg" alt="Los Angeles">
           </div>
         </div>
+        <div class="row">
+        <div class="col-6 col-lg-6">
+              <div class="embed-responsive embed-responsive-21by9 mt-5 mb-5">
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/8QxxzB6TYpo"></iframe>
+            </div>
+          </div>
+          <div class="col-6 col-lg-6">
+              <div class="embed-responsive embed-responsive-21by9 mt-5 mb-5">
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/8QxxzB6TYpo"></iframe>
+            </div>
+          </div>
+          </div>
       </div>
     </section>
     
@@ -270,3 +279,7 @@
     <!--</div>-->
 
   </div>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+ 
