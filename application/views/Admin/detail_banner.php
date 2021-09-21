c871facbae048f0aa2acb104966ea85e32b9bec8
krisnaxyoga <yogakris.yk@gmail.com>
2021-09-21 00:40:17 -0700

    detail banner

--- application/views/Admin/detail_banner.php
+++ application/views/Admin/detail_banner.php
@@ -21,23 +21,24 @@
               <div class="card-body box-profile">
               <div class="text-center">
                     <?php
-                        if (file_exists(base_url('upload/members/'.$banner->picture))) {
+                        if (file_exists(base_url('upload/members/'))) {
                             $thumb = base_url('upload/no_image/no_image.png');
                         }else{
-                            $thumb = base_url('upload/members/'.$banner->picture);
+                            $thumb = base_url('upload/members/');
                         }
                     ?>
-                  <img class="profile-user-img img-fluid img-circle"
+                  <img class="profile-user-img img-fluid"
                        src="<?php echo $thumb ?>"
                        alt="User profile picture">
                 </div>
                 
-                <h3 class="profile-username text-center"><?php echo $banner->name ?></h3>
-                <p hidden id="order_id"><?php echo $banner->id ?></p>
+                <h3 class="profile-username text-center"><?php //echo $banner->name ?></h3>
+                <p hidden id="order_id"><?php //echo $banner->id ?></p>
 
                 <ul class="list-group list-group-unbordered mb-3">
                 <li class="list-group-item">
-                    <b>picture</b> <a class="float-right"><?php echo $banner->picture ?></a>
+                    <b>picture</b> <a class="float-right"><?php //echo $banner->picture ?></a>
+                    <input Type="file" id="usdt_wallet" class="float-right" value="<?php //echo $member->usdt_wallet ?>">
                   </li>
                 </ul>
                
