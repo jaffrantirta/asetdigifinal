c01feefbef0ec532a17cfae70dc8a2a217a157b4
Iwayanyogakrisna <oxygenjuno@gmail.com>
2021-10-03 08:14:58 +0800

    tampilan jumlah turn over bonus

--- application/views/Customer/bonus_detail_turnover.php
+++ application/views/Customer/bonus_detail_turnover.php
@@ -13,54 +13,94 @@
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
+    <section>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-4 col-4">
+            <div class="small-box" style="background-color:#9c743e;">
+              <div class="inner">
+                <h3 class="text-light" id="amount"></h3>
+                <p class="text-light">GOLD</p>
+              </div>
+
+              <!-- <a class="small-box-footer"></a><i class="fas fa-arrow-circle-right"></i></a> -->
+            </div>
+          </div>
+          <div class="col-lg-4 col-4">
+            <div class="small-box" style="background-color:#9c743e;">
+              <div class="inner">
+                <h3 class="text-light" id="amount"></h3>
+                <p class="text-light">ONYK</p>
+              </div>
+
+              <!-- <a class="small-box-footer"></a><i class="fas fa-arrow-circle-right"></i></a> -->
+            </div>
+          </div>
+          <div class="col-lg-4 col-4">
+            <div class="small-box" style="background-color:#9c743e;">
+              <div class="inner">
+                <h3 class="text-light" id="amount"></h3>
+                <p class="text-light">DIAMOND</p>
+              </div>
+
+              <!-- <a class="small-box-footer"></a><i class="fas fa-arrow-circle-right"></i></a> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <p hidden id='link'>datatable/get_bonus_turnover/<?php echo $id_and_position ?></p>
     <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">All bonuses of <?php echo $position_turnover ?></h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body table-responsive">
-                <table id="table" class="table table-bordered table-striped">
-                  <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Bonus Date</th>
-                    <th>Position</th>
-                    <th>Licence Name</th>
-                    <th>Licence Price (USDT)</th>
-                    <th>Percentage Bonus (%)</th>
-                    <th>Bonus (USDT)</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
-            </div>
-<script src="<?php echo base_url() ?>assets/build/js/customer/Jquery3Offline.js"></script>
-<script>
-    $(document).ready(function() {
+      <div class="card-header">
+        <h3 class="card-title">All bonuses of <?php echo $position_turnover ?></h3>
+      </div>
+      <!-- /.card-header -->
+      <div class="card-body table-responsive">
+        <table id="table" class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Bonus Date</th>
+              <th>Position</th>
+              <th>Licence Name</th>
+              <th>Licence Price (USDT)</th>
+              <th>Percentage Bonus (%)</th>
+              <th>Bonus (USDT)</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <script src="<?php echo base_url() ?>assets/build/js/customer/Jquery3Offline.js"></script>
+    <script>
+      $(document).ready(function() {
         var link = document.getElementById('base_url').innerHTML + document.getElementById('link').innerHTML;
         console.log(link);
         table = $('#table').DataTable({
-            "responsive": true,
-            "processing": true,
-            "serverSide": true,
-            "ajax": link,
-            "bSort":true,
-            "bPaginate": true,
-            "iDisplayLength": 10,
-            "order": [[ 1, "desc" ]],
-            "language": {
-                "searchPlaceholder": "Search...",
-                "search":""
-            },
-            "fnInitComplete": function(oSettings, json) {
-                $('#table_filter :input').addClass('form-control').css({'width':'10em'});
-            }
+          "responsive": true,
+          "processing": true,
+          "serverSide": true,
+          "ajax": link,
+          "bSort": true,
+          "bPaginate": true,
+          "iDisplayLength": 10,
+          "order": [
+            [1, "desc"]
+          ],
+          "language": {
+            "searchPlaceholder": "Search...",
+            "search": ""
+          },
+          "fnInitComplete": function(oSettings, json) {
+            $('#table_filter :input').addClass('form-control').css({
+              'width': '10em'
+            });
+          }
         });
-        table.column( 6 ).visible( false );
-        table.column( 5 ).visible( false );
-    });
-</script>
+        table.column(6).visible(false);
+        table.column(5).visible(false);
+      });
+    </script>
